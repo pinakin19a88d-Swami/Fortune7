9344221dfa30256d680f49ed4a682828707717e3
Chinmay Patil <chinmay412@gmail.com>
2014-11-09 13:28:44 +0530

    ajax functionality working. now creating all ajax html pages

--- js/ajax_script.js
+++ js/ajax_script.js
@@ -1,33 +1,17 @@
 $(document).ready(function(){
 
-	$(".subMenu li a").click(function() {
+	$(".aboutUsAjax li a, .aboutUsNav li a, #aboutUs").click(function() {
 
-		$(".subMenu li a").removeClass("active");
-		$(".services span").removeClass("active");
-		$(this).addClass("active");
+		// $(".subMenu li a").removeClass("active");
+		// $(".services span").removeClass("active");
+		// $(this).addClass("active");
+
+		$(".aboutUsAjax").css("display","block");
 
 		var current = $(this).attr("name");
 		$(".pageContent").html("");
-		$(".pageContent").load("ajax/cssLoader.html");
+		// $(".pageContent").load("ajax/cssLoader.html");
 		$(".pageContent").load(current);
-
-		window.setTimeout(function() {  
-         		$( "#closeBtn" ).addClass("close");
-			    }, 100);
-
-		if(!$("#aboutNav > .subMenu li a").hasClass("active"))
-  		{
-    		$("#aboutNav > .subMenu").css("height","0");
-    		$("#aboutNav > .subMenu").css("opacity","0");
-    	}
-
-	    if(!$("#portfolioNav > .subMenu li a").hasClass("active"))
-  		{
-    		$("#portfolioNav > .subMenu").css("height","0");
-    		$("#portfolioNav > .subMenu").css("opacity","0");
-    	}
-
-    	$("#backBtn").css("display","none");
 
 	});
 
