68012201fb5a998c58c3f5513721469b64b36749
Chinmay Patil <chinmay412@gmail.com>
2014-12-03 11:52:38 +0530

    tweaks to the main navigation code. added animations to Logo and main navigation.

--- js/common_script.js
+++ js/common_script.js
@@ -3,4 +3,14 @@ $(document).ready(function(){
 	$(".mainNav .nav li").removeClass("active");
 	$(".subNavList li").removeClass("active");
 
+
+	$("#aboutUs, #services, #industry").hover(function(){
+
+		$(".mainNav .nav").css("z-index","500");
+
+	}, function(){
+
+		$(".mainNav .nav").css("z-index","200");
+
+	});
 });
