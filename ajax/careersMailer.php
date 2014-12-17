fb7cc2361754a7f3fee70b8fba479f86cafc0db3
Chinmay Patil <chinmay412@gmail.com>
2014-12-17 14:21:42 +0530

    corrections done - for 16_Dec_2014

--- ajax/careersMailer.php
+++ ajax/careersMailer.php
@@ -53,7 +53,7 @@
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
 
-        $recipient = "rajeev@fortuneps.com";
+        $recipient = "fps1@fortuneps.com";
 
         // Set the email subject.
         $subject = "New Careers Form from - $name";
@@ -76,8 +76,8 @@
 
         // Build the email headers.
         // $email_headers = "From: $nameOfCandidate <$email>";
-        $email_headers .= "Reply-To: <rajeev@fortuneps.com>\r\n"; 
-        $email_headers .= "Return-Path: <rajeev@fortuneps.com>\r\n"; 
+        $email_headers .= "Reply-To: <fps1@fortuneps.com>\r\n"; 
+        $email_headers .= "Return-Path: <fps1@fortuneps.com>\r\n"; 
         $email_headers .= "From: $name <$email>\r\n"; 
         $email_headers .= "Organization: My Organization\r\n"; 
         $email_headers .= "Content-Type: text/plain\r\n";
